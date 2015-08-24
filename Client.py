0a0b22c48825a4a49a700ec62c2a546248782a96
tkpb <thiagokalleb@gmail.com>
2015-08-24 01:49:08 -0300

    Define and insert constants

--- Client.py
+++ Client.py
@@ -1,10 +1,16 @@
 import socket
 
+HOST = 'localhost'
+PORT = 9000
+MAX_CLIENTS = 5
+BUF_SIZE = 1024
+DEFAULT_ENCODING = 'ascii'
+
 if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("localhost", 9000))
+    sock.connect((HOST, PORT))
     data = "some data"
     sock.sendall(data)
-    result = sock.recv(1024)
+    result = sock.recv(BUF_SIZE)
     print(result)
     sock.close()

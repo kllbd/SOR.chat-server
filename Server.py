3e25a68e378e4f49dfd38fe3c7e3ffabbfa5f5cf
tkpb <thiagokalleb@gmail.com>
2015-08-24 22:40:18 -0300

    Implement clients limit

--- Server.py
+++ Server.py
@@ -6,27 +6,33 @@ PORT = 9000
 MAX_CLIENTS = 5
 BUF_SIZE = 1024
 DEFAULT_ENCODING = 'ascii'
+clients = []
 
 
 def handle(connection, address):
-    import logging
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger("user%r" % address[1])
-    try:
-        logger.debug("Connected %r at %r", connection, address)
-        while True:
-            data = connection.recv(BUF_SIZE).decode(DEFAULT_ENCODING)
-            if data == "":
-                logger.debug("Socket closed remotely")
-                break
-            logger.debug("Received data %r", data)
-            connection.sendall(data.encode(DEFAULT_ENCODING))
-            logger.debug("Sent data")
-    except:
-        logger.exception("Problem handling request")
-    finally:
-        logger.debug("Closing socket")
+    if len(clients) >= MAX_CLIENTS:
+        connection.sendall("Server is full!".encode(DEFAULT_ENCODING))
         connection.close()
+    else:
+        clients.append('User' + str(len(clients) + 1))
+        import logging
+        logging.basicConfig(level=logging.DEBUG)
+        logger = logging.getLogger("user%r" % address[1])
+        try:
+            logger.debug("Connected %r at %r", connection, address)
+            while True:
+                data = connection.recv(BUF_SIZE).decode(DEFAULT_ENCODING)
+                if data == "":
+                    logger.debug("Socket closed remotely")
+                    break
+                logger.debug("Received data %r", data)
+                connection.sendall(data.encode(DEFAULT_ENCODING))
+                logger.debug("Sent data")
+        except:
+            logger.exception("Problem handling request")
+        finally:
+            logger.debug("Closing socket")
+            connection.close()
 
 
 class Server(object):

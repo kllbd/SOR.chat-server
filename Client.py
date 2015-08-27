c8e16bc18b9535b71292bb9ee4475e72af8381b2
tkpb <thiagokalleb@gmail.com>
2015-08-27 00:55:03 -0300

    Implement asynchronous send/receive on client

--- Client.py
+++ Client.py
@@ -1,16 +1,41 @@
 import socket
+import threading
 
 HOST = 'localhost'
 PORT = 9000
 BUF_SIZE = 1024
 DEFAULT_ENCODING = 'ascii'
+connected = False
+
+
+def get_valid_msg():
+    userinput = input()
+    while len(userinput) > 64 or len(userinput) < 1:
+        print("Message must be less than 64 characters and more than zero! Try again.")
+        userinput = input()
+    return userinput
+
+
+def recv_from_server(conn, addr):
+    try:
+        while 1:
+            data = conn.recv(BUF_SIZE).decode(DEFAULT_ENCODING)
+            print(data)
+    except:
+        connected = False
 
 if __name__ == "__main__":
     command = ''
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, PORT))
-    while command != 'exit':
-        command = input('Type your message: ')
-        if command != 'exit':
-            sock.sendall(command.encode(DEFAULT_ENCODING))
-    sock.close()
+    connected = True
+    recv_thread = threading.Thread(target=recv_from_server, args=(sock, HOST))  # Start receive thread
+    recv_thread.start()
+
+    print("Connected!")
+    try:
+        while connected:
+            msg = get_valid_msg()
+            sock.sendall(msg.encode(DEFAULT_ENCODING))
+    except:
+        connected = False
